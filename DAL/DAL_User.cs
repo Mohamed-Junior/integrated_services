69e8bc97cb88d92583a35403f6d0b05df0dac565
Mohamed-Junior <61044542+Mohamed-Junior@users.noreply.github.com>
2021-11-16 12:33:22 +0000

    Update DAL_User.cs

--- DAL/DAL_User.cs
+++ DAL/DAL_User.cs
@@ -14,18 +14,18 @@ namespace DSSGBOAdmin.Models.DAL
         // insert user
         public static long Add(User user)
         {
-            if (!CheckEntityUnicityName(user.Name,user.IdOrganization))
+            if (!CheckEntityUnicityName(user.Name, user.IdOrganization))
             {
                 throw new MyException("Erreur de la base de données", "Le nom d'utilisateur doit être unique.", "DAL");
             }
-            if(!CheckEntityUnicityEmail(user.Email, user.IdOrganization))
+            if (!CheckEntityUnicityEmail(user.Email, user.IdOrganization))
             {
                 throw new MyException("Erreur de la base de données", "L'e-mail d'utilisateur doit être unique.", "DAL");
             }
             using (SqlConnection con = DBConnection.GetAuthConnection())
             {
                 string StrSQL = "INSERT INTO [User] (Name,Email,PassWord,IdOrganization,Role,AccountCreationDate) " +
-                    " output INSERTED.ID " +
+                    " output INSERTED.Id " +
                     " VALUES(@Name,@Email,@PassWord,@IdOrganization,@Role,@AccountCreationDate)";
                 SqlCommand command = new SqlCommand(StrSQL, con);
                 command.Parameters.Add("@Name", SqlDbType.NVarChar).Value = user.Name;
@@ -36,49 +36,22 @@ namespace DSSGBOAdmin.Models.DAL
                 command.Parameters.Add("@AccountCreationDate", SqlDbType.Date).Value = user.AccountCreationDate;
                 return Convert.ToInt64(DataBaseAccessUtilities.ScalarRequest(command));
             }
-            //using (SqlConnection con = DBConnection.GetAuthConnection())
-            //{
-            //    string StrSQL = "INSERT INTO [User] (Name,Email,PassWord,IdOrganization,Role,AccountCreationDate) VALUES(@Name,@Email,@PassWord,@IdOrganization,@Role,@AccountCreationDate)";
-            //    SqlCommand command = new SqlCommand(StrSQL, con);
-            //    command.Parameters.Add("@Name", SqlDbType.NVarChar).Value = user.Name;
-            //    command.Parameters.Add("@Email", SqlDbType.NVarChar).Value = user.Email;
-            //    command.Parameters.Add("@PassWord", SqlDbType.NVarChar).Value = ProtectPassword(user.PassWord);
-            //    command.Parameters.Add("@IdOrganization", SqlDbType.BigInt).Value = user.IdOrganization;
-            //    command.Parameters.Add("@Role", SqlDbType.NVarChar).Value = user.Role;
-            //    command.Parameters.Add("@AccountCreationDate", SqlDbType.Date).Value = user.AccountCreationDate;
-            //    return Convert.ToInt64(DataBaseAccessUtilities.ScalarRequest(command));
-            //}
         }
-        //// cryptage Password By RSA triple DES ALG
-        //public static string ProtectPassword(string clearPassword)
-        //{
-        //    byte[] bytes = Encoding.UTF8.GetBytes(clearPassword);
-        //    byte[] protectedBytes = ProtectedData.Protect(bytes, null, DataProtectionScope.CurrentUser);
-        //    //string test = Convert.ToBase64String(protectedBytes);
-        //    //System.Diagnostics.Debug.WriteLine("testadduserhash=" + test);
-        //    return Convert.ToBase64String(protectedBytes);
-        //}
-        //// Decryptage Password By RSA triple DES ALG
-        //public static string UnprotectPassword(string protectedPassword)
-        //{
-        //    //protectedPassword = "AQAAANCMnd8BFdERjHoAwE/Cl+sBAAAAKyXeKmq/d0uEQU/YuOTLSQAAAAACAAAAAAAQZgAAAAEAACAAAAAcIVvrAum2N+96RlSy6uye6PykhxM/27vh8iVZO/fYpwAAAAAOgAAAAAIAACAAAAAkdmeBqT/dQeGqsoqD269WCY3b+fyS18s1KnW59j2VABAAAABHFhhwskaKkfwUrUeS9J3NQAAAAL+dlQJquC6k8eoO22sr+Whz56e7L8PHv3KRKUmM+MdImyQpF8R7WzPAIV5Ll7zrOlHw0gyIV10ix3mxXxeVgOg=";
-        //    byte[] protectedBytes = Convert.FromBase64String(protectedPassword);
-        //    byte[] bytes = ProtectedData.Unprotect(protectedBytes, null, DataProtectionScope.CurrentUser);
-        //    //System.Diagnostics.Debug.WriteLine("adduserdecryptage=" + Encoding.UTF8.GetString(bytes));
-        //    return Encoding.UTF8.GetString(bytes);
-        //}
+        
         public static string ProtectPassword(string clearPassword)
         {
             string ProtectPassword = Encryption.Encrypt(clearPassword);
             //System.Diagnostics.Debug.WriteLine("Encrypt=" + ProtectPassword);
             return ProtectPassword;
         }
+       
         public static string UnprotectPassword(string protectedPassword)
         {
             string UnprotectPassword = Encryption.Decrypt(protectedPassword);
             //System.Diagnostics.Debug.WriteLine("Decrypt=" + UnprotectPassword);
             return UnprotectPassword;
         }
+       
         // update user
         public static void Update(long id, User user)
         {
@@ -109,6 +82,7 @@ namespace DSSGBOAdmin.Models.DAL
                 DataBaseAccessUtilities.NonQueryRequest(command);
             }
         }
+        
         // delete user
         public static void Delete(long id)
         {
@@ -119,6 +93,7 @@ namespace DSSGBOAdmin.Models.DAL
                 DataBaseAccessUtilities.NonQueryRequest(command);
             }
         }
+        
         // select one record of table user
         public static User SelectById(long id)
         {
@@ -156,6 +131,7 @@ namespace DSSGBOAdmin.Models.DAL
                 return user;
             }
         }
+        
         // Select all record of table user
         public static List<User> SelectAll(long IdOrganization)
         {
@@ -194,10 +170,9 @@ namespace DSSGBOAdmin.Models.DAL
                 return Users;
             }
         }
-        public static List<User> TestConnexion(string UserName,string Password, out string message)
+        
+        public static List<User> TestConnexion(string UserName, string Password, out string message)
         {
-            //var xxx = ProtectPassword("AdminENIM");
-            //System.Diagnostics.Debug.WriteLine("testadduserhash=" + xxx);
             User user;
             List<User> users = new List<User>();
             bool testconnection = false;
@@ -231,7 +206,7 @@ namespace DSSGBOAdmin.Models.DAL
                             testconnection = true;
                             //System.Diagnostics.Debug.WriteLine("user.OrganizationSystemPrefix="+ user.OrganizationSystemPrefix);
                         }
-                        
+
                     }
                     if (testconnection)
                     {
@@ -246,7 +221,7 @@ namespace DSSGBOAdmin.Models.DAL
                 {
                     message = e.Message;
                     System.Diagnostics.Debug.WriteLine("message10=" + message);
-                    new MyException(e, "Database Error", message, "DAL");     
+                    new MyException(e, "Database Error", message, "DAL");
                 }
                 finally
                 {
@@ -280,7 +255,7 @@ namespace DSSGBOAdmin.Models.DAL
                         user.Name = dataReader["Name"].ToString();
                         user.NameOrg = dataReader["NameFr"].ToString();
                         user.PassWord = UnprotectPassword(dataReader["PassWord"].ToString());
-                        user.Role = dataReader["Role"].ToString();                     
+                        user.Role = dataReader["Role"].ToString();
                         users.Add(user);
                         testconnection = true;
                     }
@@ -305,8 +280,9 @@ namespace DSSGBOAdmin.Models.DAL
                 return users;
             }
         }
+        
         // Test unicity Email
-        private static bool CheckEntityUnicityEmail(string Email,long IdOrganization)
+        private static bool CheckEntityUnicityEmail(string Email, long IdOrganization)
         {
             int ocurrencesNumber = 0;
             using (SqlConnection connection = DBConnection.GetAuthConnection())
@@ -323,7 +299,7 @@ namespace DSSGBOAdmin.Models.DAL
             else
                 return false;
         }
-        public static bool CheckEmailUnicity(string Email,long IdOrganization)
+        public static bool CheckEmailUnicity(string Email, long IdOrganization)
         {
             return CheckEntityUnicityEmail(Email, IdOrganization);
         }
@@ -349,6 +325,7 @@ namespace DSSGBOAdmin.Models.DAL
         {
             return CheckEntityUnicityName(Name, IdOrganization);
         }
+        
         // Add Method Today Calcul Number of User By Organization.
         // Select all record of table user
         public static int CountUserByOrganization(long IdOrganization)
