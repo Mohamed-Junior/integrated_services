913b1cc4bc273e5705e6c8b59a5fdc55b6a6d24b
Mohamed-Junior <61044542+Mohamed-Junior@users.noreply.github.com>
2021-11-16 09:44:27 +0000

    Update Demande.cs

--- Entities/Demande.cs
+++ Entities/Demande.cs
@@ -21,7 +21,7 @@ namespace DSSGBOAdmin.Models.Entities
             string RegDemandDateDemande, string RegDemandDecisionDemande,
             string RegDemandDecisionDateDemande, string RegDecisionCommentsDemande)
         {
-            ID = IdDemande;
+            Id = IdDemande;
             Name = NameDemande;
             Affiliation = AffiliationDemande;
             FieldOfActivity = FieldOfActivityDemande;
@@ -39,22 +39,14 @@ namespace DSSGBOAdmin.Models.Entities
             RegDemandDecision = RegDemandDecisionDemande;
             RegDemandDecisionDate = RegDemandDecisionDateDemande;
             RegDecisionComments = RegDecisionCommentsDemande;
-            /*
-            if (RegDemandDecisionDemande == 0)
-                RegDemandDecisionText = "Attends";
-            else if (RegDemandDecisionDemande == 1)
-                RegDemandDecisionText = "Accepte";
-            else if (RegDemandDecisionDemande == 2)
-                RegDemandDecisionText = "Refuse";
-            else
-                RegDemandDecisionText = "Pas de decision";
-            */
+
         }
         public Demande(Demande mDemande)
         {
             if (mDemande != null)
             {
-                ID = mDemande.ID;
+
+                Id = mDemande.Id;
                 Name = mDemande.Name;
                 Affiliation = mDemande.Affiliation;
                 FieldOfActivity = mDemande.FieldOfActivity;
@@ -76,48 +68,41 @@ namespace DSSGBOAdmin.Models.Entities
             }
         }
 
-        public long ID { get; set; }
-        [Display(Name = "Nom")]
+        public long Id { get; set; }
 
         public string Name { get; set; }
+
         public string Affiliation { get; set; }
 
-        [Display(Name = "Champs d'Activité")]
-
         public string FieldOfActivity { get; set; }
-        [Display(Name = "Addresse")]
+
         public string Adress { get; set; }
-        [Display(Name = "Code Postal")]
+
         public string PostalCode { get; set; }
-        [Display(Name = "Ville")]
+
         public string City { get; set; }
-        [Display(Name = "Pays")]
+
         public string Country { get; set; }
-        [Display(Name = "Email")]
+
         public string Email { get; set; }
+
         public string Phone { get; set; }
-        [Display(Name = "Personne à Contacter")]
+
         public string PersonToContact { get; set; }
 
-        [Display(Name = "Email Contact")]
         public string ContactMail { get; set; }
 
-        [Display(Name = "Telephone Contact")]
         public string ContactPhone { get; set; }
-        [Display(Name = "Position Contact")]
+
         public string ContactPosition { get; set; }
-        [Display(Name = "Date Demande")]
+
         public string RegDemandDate { get; set; }
 
-        [Display(Name = "Decision")]
         public string RegDemandDecision { get; set; }
 
-        [Display(Name = "Date de Decision")]
         public string RegDemandDecisionDate { get; set; }
 
-        [Display(Name = "Motif de Decision")]
         public string RegDecisionComments { get; set; }
-        public string StatusActivationEmail { get; set; }
-        public string Token { get; set; }
+
     }
 }
