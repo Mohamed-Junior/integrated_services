f0a5d8d709a315cde26fb5a11205b6f59fac165c
Mohamed-Junior <61044542+Mohamed-Junior@users.noreply.github.com>
2021-11-16 09:49:36 +0000

    Update Organization.cs

--- Entities/Organization.cs
+++ Entities/Organization.cs
@@ -31,8 +31,9 @@ namespace DSSGBOAdmin.Models.Entities
         public string AccountStatus { get; set; }
         public string AccountType { get; set; }
         public string OrganizationSystemPrefix { get; set; }
-        public Organization(){}
-        public Organization(long Id, string NameFr,string NameAr,string Acronym,string OrganisationLogo,string Affiliation,string AffiliationLogo,string FieldOfActivity,string Adress,string PostalCode,string City,string Country,string Email,string Phone,string PersonToContact,string ContactMail,string ContactPhone,string ContactPosition,string ParDiffusionEmail,string ParDiffusionEmailPW,string ParOutgoingMailChar, string ParIngoingMailChar,string AccountStatus,string AccountType,string OrganizationSystemPrefix) {
+        public Organization() { }
+        public Organization(long Id, string NameFr, string NameAr, string Acronym, string OrganisationLogo, string Affiliation, string AffiliationLogo, string FieldOfActivity, string Adress, string PostalCode, string City, string Country, string Email, string Phone, string PersonToContact, string ContactMail, string ContactPhone, string ContactPosition, string ParDiffusionEmail, string ParDiffusionEmailPW, string ParOutgoingMailChar, string ParIngoingMailChar, string AccountStatus, string AccountType, string OrganizationSystemPrefix)
+        {
 
             this.Id = Id;
             this.NameFr = NameFr;
